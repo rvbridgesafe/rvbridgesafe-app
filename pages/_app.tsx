016912e6a9af1364bb534040479e501b14072876
rvbridgesafe <grannysroute@gmail.com>
2026-05-01 14:50:48 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,14 @@
 
+import type { AppProps } from 'next/app'
+import Footer from '../components/Footer'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="min-h-screen flex flex-col bg-stone-50 text-stone-800">
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  )
+}
